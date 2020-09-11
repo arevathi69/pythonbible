861502d8ad6b57d11a0b260fd60fe2fae8aec6bc
Revathi <66195488+arevathi69@users.noreply.github.com>
2020-09-11 19:15:03 +0530

    This program prints the area of a circle and  also the extension of a filename

---
+++ ar_circle.py
@@ -0,0 +1,7 @@
+#program to calculate the area of a circle
+
+
+r=1.1
+print("The radius of the circle is",r)
+ar=float((22/7)*r*r)
+print("The area of the circle with radius is ",ar)
---
+++ file_extension.py
@@ -0,0 +1,6 @@
+c=str(abc)
+d=py
+e=abc+'.'+py
+print("filename",e)
+extension=python
+print("extension of the e is",extension)
